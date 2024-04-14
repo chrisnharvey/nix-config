88d2345b5e0f3c35c48a312bd54a9267a344a882
Chris Harvey <chris@chrisnharvey.com>
2024-04-14 21:47:34 +0100

    install git and wget

--- server/configuration.nix
+++ server/configuration.nix
@@ -81,9 +81,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    pkgs.k3s
+  #  pkgs.k3s
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    wget
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
