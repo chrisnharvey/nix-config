a717404d530e4f7458be14abb389a5dfb1cb2a31
Chris Harvey <chris@chrisnharvey.com>
2024-04-17 21:31:39 +0100

    update hardware config

--- dell-laptop/hardware-configuration.nix
+++ dell-laptop/hardware-configuration.nix
@@ -13,34 +13,16 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/0d4b0f35-e187-455c-b97d-aa7a203e500d";
-      fsType = "btrfs";
-      options = [ "subvol=@" ];
-    };
-
-  boot.initrd.luks.devices."luks-c1e64d42-0251-4019-a975-0f687633c00f".device = "/dev/disk/by-uuid/c1e64d42-0251-4019-a975-0f687633c00f";
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/AD7B-A652";
-      fsType = "vfat";
-    };
-
-  fileSystems."/var/lib/docker/btrfs" =
-    { device = "/@/var/lib/docker/btrfs";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-  swapDevices = [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-b49119652bd6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-ec6750a4b18b.useDHCP = lib.mkDefault true;
   # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.veth25cd2cc.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
