4e6f812cc9d9e455d8613dd08a939615c0855eb8
Chris Harvey <chris@chrisnharvey.com>
2024-04-17 21:31:59 +0100

    add server filesystems

---
+++ server/filesystems.nix
@@ -0,0 +1,18 @@
+{
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/NIXROOT";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/NIXBOOT";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 16*1024;
+    randomEncryption.enable = true; 
+  } ];
+}
