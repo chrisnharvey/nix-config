96f0910513b288ea17ed918d4d6e9e7c6d124369
Chris Harvey <chris@chrisnharvey.com>
2024-05-06 22:41:37 +0100

    Move home to to common

---
+++ common/home.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+{
+    /* The home.stateVersion option does not have a default and must be set */
+    home.stateVersion = "23.11";
+    /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+
+    programs.starship.enable = true;
+
+    programs.zsh = {
+        enable = true;
+        autosuggestion.enable = true;
+        enableCompletion = true;
+        oh-my-zsh = {
+          enable = true;
+          plugins = ["git" "command-not-found" "docker" "docker-compose" "emoji" "git-auto-fetch" "gh" "genpass" "golang" "starship"];
+          theme = "robbyrussell";
+        };
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "Chris Harvey";
+      userEmail = "chris@chrisnharvey.com";
+      signing.key = "0B5B154A0538BD357EE58189024E65A3F7E92A36";
+      signing.signByDefault = true;
+    };
+}
--- dell-laptop/home.nix
+++ dell-laptop/home.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 {
-
+  imports =
+    [
+      ../common/home.nix
+    ];
 
     programs.plasma = {
       enable = true;
@@ -21,32 +24,5 @@
         "kwinrc"."Effect-overview"."BorderActivate".value = 9;
         "kwinrc"."Effect-windowview"."BorderActivateAll".value = 9;
       };
-    };
-
-    /* The home.stateVersion option does not have a default and must be set */
-    home.stateVersion = "23.11";
-    /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
-
-    #programs.plasma.workspace.clickItemTo = "select";
-
-    programs.starship.enable = true;
-
-    programs.zsh = {
-        enable = true;
-        autosuggestion.enable = true;
-        enableCompletion = true;
-        oh-my-zsh = {
-          enable = true;
-          plugins = ["git" "command-not-found" "docker" "docker-compose" "emoji" "git-auto-fetch" "gh" "genpass" "golang" "starship"];
-          theme = "robbyrussell";
-        };
-    };
-
-    programs.git = {
-      enable = true;
-      userName = "Chris Harvey";
-      userEmail = "chris@chrisnharvey.com";
-      signing.key = "0B5B154A0538BD357EE58189024E65A3F7E92A36";
-      signing.signByDefault = true;
     };
 }
