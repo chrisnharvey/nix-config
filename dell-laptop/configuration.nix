5174d9a693b950730db15a7fec2bc48e59021d62
Chris Harvey <chris@chrisnharvey.com>
2024-09-17 20:59:31 +0100

    remove redundant packages

--- dell-laptop/configuration.nix
+++ dell-laptop/configuration.nix
@@ -184,8 +184,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     sshfs-fuse
     htop
     keybase
@@ -201,7 +199,6 @@
     gnupg
     pinentry-qt
     unrar
-    kdePackages.full
   ];
 
   services.flatpak.enable = true;
