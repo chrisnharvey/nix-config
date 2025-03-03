3e1f826ec4dda03978ddf486c4f86215ac761e61
Chris Harvey <chris@chrisnharvey.com>
2025-03-03 19:01:33 +0000

    Update home manager state versions

--- common/home.nix
+++ common/home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
     /* The home.stateVersion option does not have a default and must be set */
-    home.stateVersion = "24.11";
+    home.stateVersion = "24.05";
     /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
 
     programs.starship.enable = true;
