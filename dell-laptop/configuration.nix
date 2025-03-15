353a3cd9ca3efde19845cab75e9a7ce638cd471f
Chris Harvey <chris@chrisnharvey.com>
2025-03-15 14:35:01 +0000

    Update pulseaudio references

--- dell-laptop/configuration.nix
+++ dell-laptop/configuration.nix
@@ -129,7 +129,7 @@
   hardware.bluetooth.powerOnBoot = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
