560cdca170fbad9e13d86872a51cd8b90072408a
Chris Harvey <chris@chrisnharvey.com>
2025-05-16 20:25:02 +0100

    Install perf and fwts

--- systems/server/configuration.nix
+++ systems/server/configuration.nix
@@ -230,6 +230,8 @@
   environment.systemPackages = with pkgs; [
   #  pkgs.k3s
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    linuxPackages_latest.perf
+    fwts
     wget
     git
     tailscale
