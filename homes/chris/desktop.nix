7a761d767b974b99881f9807a3a0cd131995f2d7
Chris Harvey <chris@chrisnharvey.com>
2025-05-19 20:00:52 +0100

    More mime associations

--- homes/chris/desktop.nix
+++ homes/chris/desktop.nix
@@ -10,11 +10,62 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "text/html" = "app.zen_browser.zen.desktop";
-      "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
-      "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
-      "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
-      "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+    # Web
+    "text/html" = "app.zen_browser.zen.desktop";
+    "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
+    "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+    "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
+    "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+
+    # VLC for Video
+    "video/mp4"              = "org.videolan.VLC.desktop";
+    "video/x-matroska"       = "org.videolan.VLC.desktop";  # .mkv
+    "video/x-msvideo"        = "org.videolan.VLC.desktop";  # .avi
+    "video/webm"             = "org.videolan.VLC.desktop";
+    "video/quicktime"        = "org.videolan.VLC.desktop";  # .mov
+    "video/mpeg"             = "org.videolan.VLC.desktop";
+    "video/ogg"              = "org.videolan.VLC.desktop";
+    "video/x-flv"            = "org.videolan.VLC.desktop";
+    "video/3gpp"             = "org.videolan.VLC.desktop";
+    "video/3gpp2"            = "org.videolan.VLC.desktop";
+
+    # VLC for Audio
+    "audio/mpeg"             = "org.videolan.VLC.desktop";  # .mp3
+    "audio/x-wav"            = "org.videolan.VLC.desktop";  # .wav
+    "audio/ogg"              = "org.videolan.VLC.desktop";
+    "audio/flac"             = "org.videolan.VLC.desktop";
+    "audio/mp4"              = "org.videolan.VLC.desktop";
+    "audio/webm"             = "org.videolan.VLC.desktop";
+    "audio/aac"              = "org.videolan.VLC.desktop";
+    "audio/x-ms-wma"         = "org.videolan.VLC.desktop";
+    "audio/x-matroska"       = "org.videolan.VLC.desktop";
+
+    # Playlists / Streams
+    "application/x-mpegURL"  = "org.videolan.VLC.desktop";  # .m3u8
+    "audio/x-mpegurl"        = "org.videolan.VLC.desktop";  # .m3u
+
+    # Email (Geary)
+    "x-scheme-handler/mailto" = "org.gnome.Geary.desktop";
+
+    # GNOME Image Viewer (eog) for image files
+    "image/jpeg"               = "org.gnome.eog.desktop";  # .jpg, .jpeg
+    "image/png"                = "org.gnome.eog.desktop";
+    "image/gif"                = "org.gnome.eog.desktop";
+    "image/bmp"                = "org.gnome.eog.desktop";
+    "image/tiff"               = "org.gnome.eog.desktop";  # .tif
+    "image/webp"               = "org.gnome.eog.desktop";
+    "image/x-icon"             = "org.gnome.eog.desktop";  # .ico
+    "image/svg+xml"            = "org.gnome.eog.desktop";  # SVG vector image
+    "image/x-xbitmap"          = "org.gnome.eog.desktop";  # .xbm
+    "image/x-portable-bitmap"  = "org.gnome.eog.desktop";  # .pbm
+    "image/x-portable-graymap" = "org.gnome.eog.desktop";  # .pgm
+    "image/x-portable-pixmap"  = "org.gnome.eog.desktop";  # .ppm
+    "image/x-xpixmap"          = "org.gnome.eog.desktop";  # .xpm
+
+    # Calendars (GNOME Calendar)
+    "text/calendar" = "org.gnome.Calendar.desktop";              # .ics files
+    "application/x-ical" = "org.gnome.Calendar.desktop";         # alternate calendar MIME
+    "x-scheme-handler/webcal" = "org.gnome.Calendar.desktop";    # webcal:// links
     };
   };
 
