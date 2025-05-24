505d59670697bdaa7c0f4e89ece39209184a4df2
Chris Harvey <chris@chrisnharvey.com>
2025-05-24 20:04:45 +0100

    Update start-homelab script

--- systems/server/scripts/start-homelab.sh
+++ systems/server/scripts/start-homelab.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-zfs-multi-mount data/data data/vms data/docker
+zfs-multi-mount data/data vms/vms vms/docker
 systemctl restart docker.service
 systemctl start duplicacy-web.service
 virsh -c qemu:///system start hass
