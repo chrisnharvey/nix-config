616da6e2afcbd2bd7fa970002fb3ec95ee9e7ae7
Chris Harvey <chris@chrisnharvey.com>
2025-06-28 11:27:02 +0100

    Add kernel param to potentially fix graphics issue

--- systems/dell-laptop/configuration.nix
+++ systems/dell-laptop/configuration.nix
@@ -60,7 +60,7 @@
   boot.initrd.verbose = false;
   boot.consoleLogLevel = 0;
   boot.resumeDevice = "/dev/mapper/ROOT";
-  boot.kernelParams = [ "quiet" "udev.log_level=0" "resume_offset=29921280" ];
+  boot.kernelParams = [ "quiet" "i915.enable_psr=0" "udev.log_level=0" "resume_offset=29921280" ];
   boot.blacklistedKernelModules = [ "intel_hid" "psmouse" ];
 
   security.tpm2.enable = true;
