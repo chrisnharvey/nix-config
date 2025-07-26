8aeb29550dec950bf79092a31913f38bf07e5939
Chris Harvey <chris@chrisnharvey.com>
2025-07-26 23:27:38 +0100

    SDDM

--- desktops/hyprland.nix
+++ desktops/hyprland.nix
@@ -1,9 +1,19 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, environment, ... }:
+{ 
     programs.hyprland = {
         enable = true;
         xwayland.enable = true;
     };
+
+    services.displayManager.sddm.enable = true;
+    services.displayManager.sddm.wayland.enable = true;
+    services.displayManager.sddm.package = pkgs.kdePackages.sddm;
+    services.displayManager.sddm.theme = "sddm-astronaut-theme";
+    services.displayManager.sddm.extraPackages = with pkgs; [
+        sddm-astronaut
+    ];
+
+    services.power-profiles-daemon.enable = true;
 
     xdg.portal.enable = true;
     xdg.portal.extraPortals = [
@@ -13,6 +23,7 @@
     environment.systemPackages = with pkgs; [
       bibata-cursors
       papirus-icon-theme
+      sddm-astronaut
     ];
 
     services.blueman.enable = true;
