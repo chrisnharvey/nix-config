3bf6161483edfd691bd661a07f81caa0379250eb
Chris Harvey <chris@chrisnharvey.com>
2025-10-19 10:43:42 +0100

    Set app name when calling notify-send

--- homes/chris/scripts/lid-switch.sh
+++ homes/chris/scripts/lid-switch.sh
@@ -6,14 +6,14 @@ MONITOR_COUNT=$(wlr-randr | grep Enabled | wc -l)
 if [ $MONITOR_COUNT -gt 1 ] && [ "$LID_STATE" = "close" ]; then
     # Multiple monitors detected and lid is closed, turn off the internal display
     wlr-randr --output eDP-1 --off
-    notify-send "Lid closed" "Turning off internal display"
+    notify-send "Lid closed" "Turning off internal display" --app-name="Lid Switch"
     exit 0
 fi
 
 if [ $MONITOR_COUNT -gt 1 ] && [ "$LID_STATE" = "open" ]; then
     # Lid opened, turn on the internal display
     wlr-randr --output eDP-1 --on
-    notify-send "Lid opened" "Turning on internal display"
+    notify-send "Lid opened" "Turning on internal display" --app-name="Lid Switch"
     exit 0
 fi
 
