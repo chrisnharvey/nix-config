bfb8b03fa035e7c2300105b9df6e1e1af098d41b
Chris Harvey <chris@chrisnharvey.com>
2025-10-23 18:35:52 +0100

    Use common niri config file with additions added per system

--- systems/dell-laptop/homes/chris/niri.nix
+++ systems/dell-laptop/homes/chris/niri.nix
@@ -5,5 +5,8 @@
   ];
 
   home.file.".config/niri/config.kdl".enable = true;
-  home.file.".config/niri/config.kdl".source = ./config/niri/config.kdl;
+  home.file.".config/niri/config.kdl".text = builtins.readFile ./config/niri/config.kdl + builtins.readFile ../../../../homes/chris/config/niri/common.kdl;
+
+#  home.file.".config/niri/config.kdl".enable = true;
+#  home.file.".config/niri/config.kdl".source = ./config/niri/config.kdl;
 }
