7668af061130ab5010b5ecef5fa6539614b8d9e2
Chris Harvey <chris@chrisnharvey.com>
2025-10-24 22:34:02 +0100

    Enable nix-ld

--- systems/steamdeck/configuration.nix
+++ systems/steamdeck/configuration.nix
@@ -303,6 +303,89 @@
   programs.zsh.enable = true;
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+
+    # Checked in studio.sh
+    coreutils
+    findutils
+    gnugrep
+    which
+    gnused
+
+    # For Android emulator
+    file
+    mesa-demos
+    pciutils
+    xorg.setxkbmap
+
+    # Used during setup wizard
+    gnutar
+    gzip
+
+    # Runtime stuff
+    git
+    ps
+    usbutils
+
+    # Crash at startup without these
+    fontconfig
+    freetype
+    libxext
+    xorg.libXi
+    libxrender
+    xorg.libXtst
+
+    # No crash, but attempted to load at startup
+    e2fsprogs
+
+    # Gradle wants libstdc++.so.6
+    (lib.getLib stdenv.cc.cc)
+    # mksdcard wants 32 bit libstdc++.so.6
+    pkgsi686Linux.stdenv.cc.cc.lib
+
+    # aapt wants libz.so.1
+    zlib
+    pkgsi686Linux.zlib
+    # Support multiple monitors
+    libxrandr
+
+    # For Android emulator
+    alsa-lib
+    dbus
+    expat
+    libbsd
+    libpulseaudio
+    libuuid
+    libx11
+    libxcb
+    libxkbcommon
+    libxcb-wm
+    libxcb-render-util
+    libxcb-keysyms
+    libxcb-image
+    xcb-util-cursor
+    xorg.libICE
+    xorg.libSM
+    xorg.libxkbfile
+    xorg.libXcomposite
+    libxcursor
+    xorg.libXdamage
+    libxfixes
+    libGL
+    libdrm
+    libpng
+    nspr
+    nss_latest
+    systemd
+
+    # For GTKLookAndFeel
+    gtk2
+    glib
+
+    # For wayland support
+    wayland
+  ];
 
   programs.gnupg.agent = {
     enable = true;
