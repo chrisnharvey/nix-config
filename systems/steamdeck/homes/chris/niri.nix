c51390dd1c22472227392c4c616a48f3af063ca8
Chris Harvey <chris@chrisnharvey.com>
2025-12-06 17:42:20 +0000

    Format

--- systems/steamdeck/homes/chris/niri.nix
+++ systems/steamdeck/homes/chris/niri.nix
@@ -8,5 +8,6 @@
   home.file.".config/niri/config.kdl".text = builtins.readFile ./config/niri/config.kdl;
 
   home.file.".config/niri/common.kdl".enable = true;
-  home.file.".config/niri/common.kdl".text = builtins.readFile ../../../../homes/chris/config/niri/common.kdl;
+  home.file.".config/niri/common.kdl".text =
+    builtins.readFile ../../../../homes/chris/config/niri/common.kdl;
 }
