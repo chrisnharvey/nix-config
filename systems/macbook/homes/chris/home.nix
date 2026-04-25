62aa02a7527dab17f620da1251c6e6225ac2ff96
Chris Harvey <chris@chrisnharvey.com>
2026-04-25 16:33:57 +0100

    Android dev env setup

--- systems/macbook/homes/chris/home.nix
+++ systems/macbook/homes/chris/home.nix
@@ -10,6 +10,17 @@
     '';
   };
 
+  home.sessionVariables = {
+    ANDROID_HOME = "${config.home.homeDirectory}/Library/Android/sdk";
+  };
+
+  home.sessionPath = [
+    "/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/bin"
+    "${config.home.homeDirectory}/Library/Android/sdk/platform-tools"
+    "${config.home.homeDirectory}/Library/Android/sdk/emulator"
+    "${config.home.homeDirectory}/Library/Android/sdk/cmdline-tools/latest/bin"
+  ];
+
   home.packages = with pkgs; [
 
   ];
